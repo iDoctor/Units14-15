4dbb17b9983ef0a18d84ebbb074c9bd57ddd2e05
Roman Filatov <ikardoc@gmail.com>
2022-05-21 14:13:33 +0300

    14.3.3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14.3.3
+
+var phoneBook = new List<Contact>();
+
+// добавляем контакты
+phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+var sortedPgoneBookList = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName).ToList();
+foreach (var contact in sortedPgoneBookList)
+{
+    Console.WriteLine($"{contact.Name} {contact.LastName}: '{contact.PhoneNumber}'");
+}
+
+public class Contact // модель класса
+{
+    public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+    {
+        Name = name;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public String Name { get; }
+    public String LastName { get; }
+    public long PhoneNumber { get; }
+    public String Email { get; }
+}
